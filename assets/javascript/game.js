22fd7b2331d59ed481679e1e4041bdaf71929720
Brian Dawkins <bkdawkins@gmail.com>
2018-03-06 18:48:47 -0800

    game.js beginning qction correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,21 @@
 
 var hangmanChoices = ["lagoon", "breeze", "island", "snorkel", "coconut", "rum", "surf", "sunscreen", "wave", "sand", "sunset"];
 var dashes = ["-", "-","-","-","-","-","-","-","-", ];
+
 var wins = 0;
 var losses = 0;
-var currentWord = null;
-var guessesRemaining = null;
-var alreadyGuessed = null;
+var currentWord = 0;
+var guessesRemaining = 0;
+var alreadyGuessed = 0;
+
 document.onkeyup = function(event) {
     var userGuess = event.key;
     var html =
-    "<p>wins: " + wins + "</p>" +
-    "<p>losses: " + losses + "</p>"
-    "<p>currentWord:</p>"
-    "<p>guessesRemaining</p>"
-    "<p>alreadyGuesses </p>";
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Current Word:" + currentWord + "</p>" +
+    "<p>Guesses Remaining:" + guessesRemaining + "</p>" +
+    "<p>Already Guessed:" + alreadyGuessed + "</p>";
    
     document.querySelector("#beginning").innerHTML = html;
 };
