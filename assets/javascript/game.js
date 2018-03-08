5980588e1e073d9867cb3e1de04f14d78e32b91c
Brian Dawkins <bkdawkins@gmail.com>
2018-03-07 22:39:23 -0800

    game.js now renders random word as dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,46 @@
 /* BEGIN JAVASCRIPT*/
 
-var hangmanChoices = ["lagoon", "breeze", "island", "snorkel", "coconut", "rum", "surf", "sunscreen", "wave", "sand", "sunset"];
-var dashes = ["-", "-","-","-","-","-","-","-","-", ];
+var hangmanArray = ["lagoon", "breeze", "island", "coconut", "sunscreen", "sunset", "snorkel"];
+
+// var hangmanChoices = {
+//     lagoon: "_ _ _ _ _ _",
+//     breeze: "_ _ _ _ _ _",
+//     island: "_ _ _ _ _ _",
+//     coconut: "_ _ _ _ _ _ _",
+//     sunscreen: "_ _ _ _ _ _ _ _ _",
+//     sunset: "_ _ _ _ _ _",
+//     snorkel: "_ _ _ _ _ _ _"
+
+
+   
+// };
+
 
 var wins = 0;
 var losses = 0;
-var currentWord = 0;
-var guessesRemaining = 0;
-var alreadyGuessed = 0;
-
+var currentWord = [];
+var guessesRemaining = 15;
+var alreadyGuessed = [];
 document.onkeyup = function(event) {
-    var userGuess = event.key;
+    // var userGuess = event.key;
+
+   
+    var chosenWord = hangmanArray[Math.floor(Math.random() * hangmanArray.length)];
+    hangmanWord = chosenWord.replace(/\D/g, "_ ");
+    console.log(chosenWord);
+    console.log(hangmanWord);
+    
+
+
     var html =
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" +
-    "<p>Current Word:" + currentWord + "</p>" +
+    "<p>Current Word:" + hangmanWord + "</p>" +
     "<p>Guesses Remaining:" + guessesRemaining + "</p>" +
-    "<p>Already Guessed:" + alreadyGuessed + "</p>";
+    "<p>Already Guessed:" + alreadyGuessed.push(); + "</p>";
 
     document.querySelector("#beginning").innerHTML = html;
-        var hangmanWord = hangmanChoices[Math.floor(Math.random() * hangmanChoices.length)];
-    console.log(hangmanWord);
+
 
 };
 
